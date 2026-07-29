c6a9947698e7d25f8aa383786273c085fc571723
TKcodes-bit <t.odongo@alustudent.com>
2026-07-29 13:30:16 +0300

    edits

--- summative/flutterapp/lib/widgets/custom_text_field.dart
+++ summative/flutterapp/lib/widgets/custom_text_field.dart
@@ -15,22 +15,27 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextFormField(
         controller: controller,
         keyboardType: keyboardType,
+        style: const TextStyle(fontSize: 16),
+
+        decoration: InputDecoration(
+          labelText: label,
+          border: const OutlineInputBorder(),
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 16,
+          ),
+        ),
+
         validator: (value) {
           if (value == null || value.trim().isEmpty) {
-            return "$label is required";
+            return '$label is required';
           }
           return null;
         },
-        decoration: InputDecoration(
-          labelText: label,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
       ),
     );
   }
