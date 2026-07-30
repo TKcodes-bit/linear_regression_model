7cba56b6a89b3a0b5764812dada8056892ff9b1e
TKcodes-bit <t.odongo@alustudent.com>
2026-07-30 11:47:51 +0300

    encoding

--- summative/flutterapp/lib/services/api_service.dart
+++ summative/flutterapp/lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import '../models/vehicle.dart';
+import '../utils/encoders.dart';
 
 class ApiService {
   // Your deployed Render API
@@ -9,20 +10,55 @@ class ApiService {
       "https://vehicle-co2-api.onrender.com";
 
   static Future<double> predictEmission(Vehicle vehicle) async {
+
+    // Encode categorical values before sending to ML API
+    final encodedVehicle = {
+      "year": vehicle.year,
+
+      "make": makeEncoder[vehicle.make],
+      "model": modelEncoder[vehicle.model],
+      "vehicle_class":
+          vehicleClassEncoder[vehicle.vehicleClass],
+
+      "engine_size": vehicle.engineSize,
+      "cylinders": vehicle.cylinders,
+
+      "transmission":
+          transmissionEncoder[vehicle.transmission],
+
+      "fuel_type":
+          fuelTypeEncoder[vehicle.fuelType],
+
+      "fuel_city": vehicle.fuelCity,
+      "fuel_hwy": vehicle.fuelHwy,
+      "fuel_comb": vehicle.fuelComb,
+      "fuel_mpg": vehicle.fuelMpg,
+    };
+
+
+    print("Sending encoded JSON:");
+    print(encodedVehicle);
+
+
     final response = await http.post(
       Uri.parse("$baseUrl/predict"),
       headers: {
         "Content-Type": "application/json",
       },
-      body: jsonEncode(vehicle.toJson()),
+      body: jsonEncode(encodedVehicle),
     );
+
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
 
       return (data["predicted_co2"] as num).toDouble();
+
     } else {
-      throw Exception("Prediction failed: ${response.body}");
+      throw Exception(
+        "Prediction failed: ${response.body}"
+      );
     }
   }
 }
+
